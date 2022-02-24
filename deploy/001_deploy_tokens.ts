5d245ea29b5f43586cd74ef0738d7075eac17e19
Vasiliy Yorkin <vasiliy.yorkin@gmail.com>
2022-02-24 15:57:39 +0300

    Finish migration to hardhat-deploy

--- deploy/001_deploy_tokens.ts
+++ deploy/001_deploy_tokens.ts
@@ -4,19 +4,22 @@ import { DeployFunction } from "hardhat-deploy/types";
 import { toUnit } from "../utils";
 
 const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
-  const { deployments, getNamedAccounts } = hre;
-  const { deploy } = deployments;
+  const {
+    deployments: { deploy },
+    getNamedAccounts,
+  } = hre;
   const { deployer } = await getNamedAccounts();
 
   const token = (name: string, desc: string) => ({
+    contract: "Token",
     from: deployer,
     args: [name, desc, toUnit(1000000)],
     log: true,
   });
 
-  await deploy("Token", token("AAA", "AAA Token"));
-  await deploy("Token", token("BBB", "BBB Token"));
-  await deploy("Token", token("CCC", "CCC Token"));
+  await deploy("AAAToken", token("AAA", "AAA Token"));
+  await deploy("BBBToken", token("BBB", "BBB Token"));
+  await deploy("CCCToken", token("CCC", "CCC Token"));
 };
 
 export default func;
--- deploy/002_deploy_exchanges.ts
+++ deploy/002_deploy_exchanges.ts
@@ -1,19 +1,25 @@
 import { HardhatRuntimeEnvironment } from "hardhat/types";
-import { DeployFunction } from "hardhat-deploy/types";
+import { DeployFunction, Address } from "hardhat-deploy/types";
 
 const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
   const { deployments, getNamedAccounts } = hre;
   const { deploy } = deployments;
   const { deployer } = await getNamedAccounts();
 
-  const exchange = (token_addr: string) => ({
+  const exchange = (token_addr: Address) => ({
+    contract: "Exchange",
     from: deployer,
     args: [token_addr],
     log: true,
   });
 
-  await deploy("Exchange", exchange(aaa.address));
-  await deploy("Exchange", exchange(bbb.address));
+  const AAA = await deployments.get("AAAToken");
+  const BBB = await deployments.get("BBBToken");
+  const CCC = await deployments.get("CCCToken");
+
+  await deploy("AAAExchange", exchange(AAA.address));
+  await deploy("BBBExchange", exchange(BBB.address));
+  await deploy("CCCExchange", exchange(CCC.address));
 };
 
 export default func;
